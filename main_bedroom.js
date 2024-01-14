5fcd66485ecf7a9ddf23215c160cf1112c66e3df
divaaga <93255055+divaaga@users.noreply.github.com>
2024-01-14 21:20:40 +0800

    Update main_bedroom.js

--- main_bedroom.js
+++ main_bedroom.js
@@ -6,9 +6,23 @@ function preload(){
 
 function setup() {
     canvas = createCanvas(660, 400);
-    canvas.center();    
+    canvas.center();  
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";  
 }
 
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
 function draw() {
     image(img, 0, 0, canvas.width, canvas.height);
     fill("red");
