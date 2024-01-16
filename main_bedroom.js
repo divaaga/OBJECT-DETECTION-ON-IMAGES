02d01c6cf0196df263b43262f588d5918307fec9
divaaga <93255055+divaaga@users.noreply.github.com>
2024-01-16 21:03:48 +0800

    Update main_bedroom.js

--- main_bedroom.js
+++ main_bedroom.js
@@ -25,12 +25,17 @@ function gotResult(error, results) {
 }
 function draw() {
     image(img, 0, 0, canvas.width, canvas.height);
-    fill("red");
-    textSize(20);
-    text("Bed", 50, 150);
-    noFill();
-    stroke("red");
-    rect(50, 150, canvas.width-200, canvas.height-150);
+    if (status != "") {
+        for (i=0; i<objects.length; i++) {
+            fill("red");
+            textSize(20);
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label + " " + percent + "%", objects[i].x + 10, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function back() {
