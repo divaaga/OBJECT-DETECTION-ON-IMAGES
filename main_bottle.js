492c93a69cafae940e99cf35e2a0388f3297000b
divaaga <93255055+divaaga@users.noreply.github.com>
2024-01-16 21:04:28 +0800

    Update main_bottle.js

--- main_bottle.js
+++ main_bottle.js
@@ -25,12 +25,17 @@ function gotResult(error, results) {
 }
 function draw() {
     image(img, 0, 0, canvas.width, canvas.height);
-    fill("red");
-    textSize(20);
-    text("Bottle", 200, 20);
-    noFill();
-    stroke("red");
-    rect(200, 20, canvas.width-400, canvas.height-40);
+    if (status != "") {
+        for (i=0; i<objects.length; i++) {
+            fill("red");
+            textSize(20);
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label + " " + percent + "%", objects[i].x + 10, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function back() {
